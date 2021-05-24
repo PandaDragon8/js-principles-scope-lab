8790e1f6ae1c1c03427dc21bebdd9fb046b86918
Min <mtan121@aucklanduni.ac.nz>
2021-05-24 21:10:05 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const LeastFavoriteCustomer ='some bob';
+
+function upperCaseCustomerName(){
+    return customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+     bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = "something else"
+}
